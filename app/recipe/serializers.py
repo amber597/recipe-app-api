39682f1045f40c22301bcc5c975302974c1b9e4e
Amber <ambersinghmio1@gmail.com>
2022-07-05 18:10:54 +0530

    added recipe api

---
+++ app/recipe/serializers.py
@@ -0,0 +1,27 @@
+"""
+Serializers for recipe APIs
+"""
+from rest_framework import serializers
+
+from core.models import (
+    Recipe,
+    # Tag,
+    # Ingredient,
+)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+    # tags = TagSerializer(many=True, required=False)
+    # ingredients = IngredientSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
